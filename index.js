79390393054e7f1d9c0fe5cc161d84816047a401
ivan <ivanlaraorta@gmail.com>
2026-03-16 16:18:37 +0100

    WS Auth completo

--- index.js
+++ index.js
@@ -3,15 +3,22 @@ const cors = require("cors");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const mongojs = require("mongojs");
+const bcrypt = require("bcrypt");
+const jwt = require("jwt-simple");
+const moment = require("moment");
 
-const config = require("./config");
+
 
 const app = express();
+
+
+const config = require("./config"); 
 
 app.use(cors());
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(express.json());
+
 
 const db = mongojs(config.DB, ["user"]);
 
@@ -61,5 +68,77 @@ app.delete("/api/user/:id", (req, res) => {
   db.user.remove({ _id: mongojs.ObjectId(req.params.id) }, (err) => {
     if (err) return res.status(500).json({ result: "KO", msg: err });
     res.json({ result: "OK" });
+  });
+});
+
+app.post("/api/auth/reg", async (req, res) => {
+  const { name, email, pass } = req.body;
+
+  if (!name || !email || !pass) {
+    return res.json({ result: "KO", msg: "Datos incompletos" });
+  }
+
+  const hash = await bcrypt.hash(pass, 10);
+
+  const user = {
+    email,
+    displayName: name,
+    password: hash,
+    signupDate: moment().unix(),
+    lastLogin: moment().unix(),
+  };
+
+  db.user.insert(user, (err, doc) => {
+    if (err) return res.json({ result: "KO", msg: err });
+
+    const payload = {
+      sub: doc._id,
+      iat: moment().unix(),
+      exp: moment().add(7, "days").unix(),
+    };
+
+    const token = jwt.encode(payload, config.SECRET);
+
+    res.json({
+      result: "OK",
+      token,
+      usuario: doc,
+    });
+  });
+});
+
+
+
+app.post("/api/auth/login", (req, res) => {
+  const { email, pass } = req.body;
+
+  db.user.findOne({ email }, async (err, user) => {
+    if (!user)
+      return res.json({
+        result: "KO",
+        msg: "Usuario o contraseña incorrectos",
+      });
+
+    const match = await bcrypt.compare(pass, user.password);
+
+    if (!match)
+      return res.json({
+        result: "KO",
+        msg: "Usuario o contraseña incorrectos",
+      });
+
+    const payload = {
+      sub: user._id,
+      iat: moment().unix(),
+      exp: moment().add(7, "days").unix(),
+    };
+
+    const token = jwt.encode(payload, config.SECRET);
+
+    res.json({
+      result: "OK",
+      token,
+      usuario: user,
+    });
   });
 });
